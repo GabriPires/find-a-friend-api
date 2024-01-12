093d81dd0374b885dec6ffd60f6702848ace20b4
GabriPires <gabpiresdev@gmail.com>
2024-01-12 14:43:10 -0300

    ✨ feat(refresh-token): route to refresh token

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,10 +3,13 @@ import { register } from './controllers/register.controller'
 import { createPet } from './controllers/create-pet.controller'
 import { authenticate } from './controllers/authenticate.controller'
 import { verifyJwt } from './middlewares/verify-jwt'
+import { refresh } from './controllers/refresh.controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/organization', register)
   app.post('/authenticate', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.post('/pet', { onRequest: [verifyJwt] }, createPet)
 }
